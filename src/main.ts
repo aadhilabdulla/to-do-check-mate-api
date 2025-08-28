25a06ccc25777cc1c97d9b8bd678c8f3cac1b73c
Aadhil Abdulla <aadhil242@gmail.com>
2025-08-28 23:06:11 +0530

    Swagger configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("ToDO CheckMate API")
+    .setDescription("API Documentation")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); 
+
+  let currentPort = process.env.PORT ?? 3000
+  await app.listen(currentPort);
+  
+  console.log(`http://localhost:${currentPort}`)
 }
 bootstrap();
