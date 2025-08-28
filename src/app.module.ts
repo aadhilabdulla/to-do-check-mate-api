c5b022bf50fb9e32a6923739dfbbc16b16880e8c
Aadhil Abdulla <aadhil242@gmail.com>
2025-08-28 23:05:44 +0530

    Configured Mongoose and Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal : true}),
+    MongooseModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory : (configService : ConfigService )=>({
+        uri : configService.get<string>('MONGO_URI')
+      })
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
